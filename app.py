c1c7edcd16b772f066687e8487ff86b1a9db6a2c
AedenMoylan <aedenmoylan@gmail.com>
2021-10-28 22:02:20 +0100

    adding basic web site that can store data to a txt file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 import datetime
 
@@ -14,5 +14,27 @@ def hello():
     return "hello bababooey"
 
 
+@app.get("/home")
+def home_page():
+    with open("home.html") as f:
+        html = f.read()
+        return html
+
+        
+@app.get("/showform")
+def display_form():
+    with open("form.html") as f:
+        html = f.read()
+    return html
+
+
+@app.post("/processform")
+def process_form():
+    the_name = request.form["thename"]
+    the_dob = request.form["thedob"]
+    with open("suckers.txt", "a") as sf:
+        print(f"{the_name}, {the_dob}", file=sf)
+    return f"Hi there, {the_name}, we know you were born on:  {the_dob}."
+
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
